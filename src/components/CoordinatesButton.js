fc22e5a70939ae7d0dc8fd3ab65d14294098bf48
Farida <faridah.absi@gmail.com>
2018-10-29 11:00:51 +0000

    automatically backed up by learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -4,7 +4,11 @@ import React from 'react';
 export default class CoordinatesButton extends React.Component {
 
   handleClick = (event) => {
+<<<<<<< HEAD
     this.props.onReceiveCoordinates([event.clientX, event.clientY]); // prints 'click'
+=======
+    this.props.onReceiveCoordinates([event.clientX, event.clientY])); // prints 'click'
+>>>>>>> 8b63d4e3b174d57409e0bb172cce2b3f2dac9b50
   }
 
   render() {
--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -4,11 +4,19 @@ import React from 'react';
 export default class DelayedButton extends React.Component {
 
   handleClick = (event) => {
+<<<<<<< HEAD
     event.persist();
   setTimeout(()=> {this.props.onDelayedClick(event)},this.props.delay)
 
   }
 
+=======
+    console.log(event.onDelayedClick()); 
+    console.log(event.delay); 
+
+  }
+  
+>>>>>>> 8b63d4e3b174d57409e0bb172cce2b3f2dac9b50
 
   render() {
       return (
